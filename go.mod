4820e2f7e8a7b18e073dab0d59b38b77b72b3f78
Denis Korkhov <denis.korkhov@discretemind.com>
2020-06-15 13:32:57 +0300

    BTCD Implementation with fixed referencies

---
+++ go.mod
@@ -0,0 +1,17 @@
+module gitlab.com/jaxnet/core/shard.core.git
+
+go 1.14
+
+require (
+	github.com/aead/siphash v1.0.1
+	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f
+	github.com/btcsuite/btcutil v1.0.2
+	github.com/btcsuite/go-socks v0.0.0-20170105172521-4720035b7bfd
+	github.com/btcsuite/goleveldb v1.0.0
+	github.com/btcsuite/websocket v0.0.0-20150119174127-31079b680792
+	github.com/davecgh/go-spew v1.1.1
+	github.com/jessevdk/go-flags v1.4.0
+	github.com/jrick/logrotate v1.0.0
+	github.com/kkdai/bstream v1.0.0
+	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9
+)
---
+++ netsync/interface.go
@@ -0,0 +1,41 @@
+// Copyright (c) 2017 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package netsync
+
+import (
+	"gitlab.com/jaxnet/core/shard.core.git/blockchain"
+	"gitlab.com/jaxnet/core/shard.core.git/btcutil"
+	"gitlab.com/jaxnet/core/shard.core.git/chaincfg"
+	"gitlab.com/jaxnet/core/shard.core.git/chaincfg/chainhash"
+	"gitlab.com/jaxnet/core/shard.core.git/mempool"
+	"gitlab.com/jaxnet/core/shard.core.git/peer"
+	"gitlab.com/jaxnet/core/shard.core.git/wire"
+)
+
+// PeerNotifier exposes methods to notify peers of status changes to
+// transactions, blocks, etc. Currently server (in the main package) implements
+// this interface.
+type PeerNotifier interface {
+	AnnounceNewTransactions(newTxs []*mempool.TxDesc)
+
+	UpdatePeerHeights(latestBlkHash *chainhash.Hash, latestHeight int32, updateSource *peer.Peer)
+
+	RelayInventory(invVect *wire.InvVect, data interface{})
+
+	TransactionConfirmed(tx *btcutil.Tx)
+}
+
+// Config is a configuration struct used to initialize a new SyncManager.
+type Config struct {
+	PeerNotifier PeerNotifier
+	Chain        *blockchain.BlockChain
+	TxMemPool    *mempool.TxPool
+	ChainParams  *chaincfg.Params
+
+	DisableCheckpoints bool
+	MaxPeers           int
+
+	FeeEstimator *mempool.FeeEstimator
+}
