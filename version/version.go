e91f0ebf743c1fc8f3579651841dd68cd8e5dfc5
Michael Popsuev <sheb.gregor@gmail.com>
2021-09-18 15:52:43 +0300

    bump version

--- version/version.go
+++ version/version.go
@@ -20,7 +20,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 0
 	appMinor uint = 3
-	appPatch uint = 13
+	appPatch uint = 14
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
